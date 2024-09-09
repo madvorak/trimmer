e1cb4b963391ec1028dffe271109a9b85108d861
madvorak <martin.dvorak@matfyz.cz>
2024-09-09 11:36:12 +0200

    update

--- Main.lean
+++ Main.lean
@@ -1,12 +1,12 @@
 open IO FS System FilePath
 
 def trimEnds (s : String) : String :=
-  (s.split (· == '\n')).map (fun l => l.dropRightWhile (· == ' ')) |> String.intercalate "\n"
+  (s.split (· == '\n')).map (·.dropRightWhile (· == ' ')) |> String.intercalate "\n"
 
 def main (args : List String) : IO Unit :=
   match args with
   | [] => println "No root directory provided!"
-  | d::_ =>
+  | d :: _ =>
     for fp in ← walkDir d do
       if fp.extension == some "lean" then
         let text ← readFile fp
